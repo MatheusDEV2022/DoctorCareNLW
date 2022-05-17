19d6a2df5620bf474a8e7650f1721c6f0005d431
Matheus Farias <101764993+MatheusDEV2022@users.noreply.github.com>
2022-05-17 12:06:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+document.addEventListener('scroll', onScroll)
+onScroll()
+function onScroll(){
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+function activateMenuAtCurrentSection(section){
+    const targetLine = scrollY + innerHeight / 2
+
+// Verificar se a Seção passou da Linha Alvo //
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+// Verificar se a Base da Seção está abaixo da Linha Alvo //
+
+    const sectionEndsAt = sectionTop + sectionHeight
+    
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+// Limites da Seção //
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
+}
+
+function showNavOnScroll(){
+    if (scrollY > 0) {
+        navigation.classList.add('scroll')
+    }
+    else{
+        navigation.classList.remove('scroll')
+    }
+}
+function showBackToTopButtonOnScroll(){
+    if (scrollY > 400) {
+        backToTopButton.classList.add('show')
+    }
+    else{
+        backToTopButton.classList.remove('show')
+    }
+}
+
+function openMenu(){
+    document.body.classList.add('menu-expanded');
+}
+function closeMenu(){
+    document.body.classList.remove('menu-expanded');
+}
+
+
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    #home, 
+    #home img, 
+    #home .stats, 
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about content`);
